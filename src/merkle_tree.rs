ecdf86e31edea39105643efc247c1632660cdb9f
Vladislav Lanskikh <nothingnesstobe@gmail.com>
2016-06-21 21:04:36 +0300

    rustfmt

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -16,24 +16,32 @@ use bincode::SizeLimit;
 /// Public Functios:
 /// fn new() -> MerklePatriciaTree<T> create new node
 /// fn get(&self, key: &'a [u8]) -> Option<&T> return value with a given key
-/// fn insert(&mut self, key: &'a [u8], Option<T>) insert given value with key into tree (panic if key exist)
-/// fn remove(&mut self, key: &'a [u8]) -> Option<T> remove (functions return None when value not exist)
+/// fn insert(&mut self, key: &'a [u8], Option<T>) insert given
+/// value with key into tree (panic if key exist)
+/// fn remove(&mut self, key: &'a [u8]) -> Option<T> remove
+/// (functions return None when value not exist)
 
-pub trait Merkle<'a, T: 'a> where T: Encodable {
+pub trait Merkle<'a, T: 'a>
+    where T: Encodable
+{
     fn new() -> MerklePatriciaTree<T>;
     fn insert(&mut self, key: &'a [u8], Option<T>);
     fn get(&self, key: &'a [u8]) -> Option<&T>;
     fn remove(&mut self, key: &'a [u8]) -> Option<T>;
 }
 
-pub struct MerklePatriciaTree<T> where T: Encodable {
+pub struct MerklePatriciaTree<T>
+    where T: Encodable
+{
     value: Option<T>,
     key: Vec<u8>,
     hash: [u8; 32],
-    children: BTreeMap<u8, MerklePatriciaTree<T>>
+    children: BTreeMap<u8, MerklePatriciaTree<T>>,
 }
 
-impl<'a, T> Merkle<'a, T> for MerklePatriciaTree<T> where T: Encodable + 'a {
+impl<'a, T> Merkle<'a, T> for MerklePatriciaTree<T>
+    where T: Encodable + 'a
+{
     fn new() -> MerklePatriciaTree<T> {
         MerklePatriciaTree::<T> {
             value: None,
@@ -48,11 +56,11 @@ impl<'a, T> Merkle<'a, T> for MerklePatriciaTree<T> where T: Encodable + 'a {
             if key.as_ref().len() == self.key.len() {
                 return self.value.as_ref();
             } else {
-                letn temp = &key.as_ref()[self.key.len()..];
-                letn mut suffix: Vec<u8> = Vec::new();
-                sufnfix.extend_from_slice(temp);
-                retnurn self.children[&suffix[0]].get(&Rc::new(suffix));
-            }n
+                let temp = &key.as_ref()[self.key.len()..];
+                let mut suffix: Vec<u8> = Vec::new();
+                suffix.extend_from_slice(temp);
+                return self.children[&suffix[0]].get(&Rc::new(suffix));
+            }
         }
         None
     }
@@ -64,8 +72,7 @@ impl<'a, T> Merkle<'a, T> for MerklePatriciaTree<T> where T: Encodable + 'a {
         } else {
             let max_matched_length = |a: &[u8], b: &[u8]| {
                 let mut count: usize = 0;
-                while count < a.as_ref().len() &&
-                    count < b.as_ref().len() && a[count] == b[count] {
+                while count < a.as_ref().len() && count < b.as_ref().len() && a[count] == b[count] {
                     count += 1;
                 }
                 count
@@ -73,14 +80,14 @@ impl<'a, T> Merkle<'a, T> for MerklePatriciaTree<T> where T: Encodable + 'a {
 
             let length = max_matched_length(&self.key, key);
 
-            if length >= self.key.len()  {
+            if length >= self.key.len() {
                 if length == key.len() {
-                    if self.value.is_some () {
+                    if self.value.is_some() {
                         panic!("key exists");
                     }
 
-                self.value = value;
-                self.update_hash();
+                    self.value = value;
+                    self.update_hash();
                 } else {
                     let temp = &key[length..];
                     let mut suffix: Vec<u8> = Vec::new();
@@ -95,10 +102,10 @@ impl<'a, T> Merkle<'a, T> for MerklePatriciaTree<T> where T: Encodable + 'a {
 
                 let temp = self.value.take();
                 let mut node: MerklePatriciaTree<T> =
-                    MerklePatriciaTree::<T>::create_node_with_args(
-                        temp,
-                        suffix, [0; 32],
-                        BTreeMap::new());
+                    MerklePatriciaTree::<T>::create_node_with_args(temp,
+                                                                   suffix,
+                                                                   [0; 32],
+                                                                   BTreeMap::new());
 
                 mem::swap(&mut node.children, &mut self.children);
                 node.update_hash();
@@ -149,8 +156,12 @@ impl<'a, T> Merkle<'a, T> for MerklePatriciaTree<T> where T: Encodable + 'a {
     }
 }
 
-impl<T> MerklePatriciaTree<T> where T: Encodable + {
-    fn create_node_with_args(value: Option<T>, key: Vec<u8>, hash: [u8; 32],
+impl<T> MerklePatriciaTree<T>
+    where T: Encodable
+{
+    fn create_node_with_args(value: Option<T>,
+                             key: Vec<u8>,
+                             hash: [u8; 32],
                              children: BTreeMap<u8, MerklePatriciaTree<T>>)
                              -> MerklePatriciaTree<T> {
         MerklePatriciaTree::<T> {
@@ -176,7 +187,7 @@ impl<T> MerklePatriciaTree<T> where T: Encodable + {
                 hasher.input(&self.key);
                 hasher.result(&mut hash);
             }
-            _ => panic!("incorrect value")
+            _ => panic!("incorrect value"),
         }
 
         out.input(&hash);
@@ -205,9 +216,8 @@ impl<T> MerklePatriciaTree<T> where T: Encodable + {
     fn try_to_compress(&mut self) {
         if self.is_empty() {
             self.key.clear();
-        }
-        else if self.value.is_none() && self.children.len() == 1 {
-            let mut new_children =  BTreeMap::new();
+        } else if self.value.is_none() && self.children.len() == 1 {
+            let mut new_children = BTreeMap::new();
             {
                 let child;
                 {
@@ -224,7 +234,7 @@ impl<T> MerklePatriciaTree<T> where T: Encodable + {
         self.update_hash();
     }
 
-        pub fn is_empty(&self) -> bool {
+    pub fn is_empty(&self) -> bool {
         self.children.is_empty() && self.value.is_none()
     }
 
@@ -244,7 +254,6 @@ impl<T> MerklePatriciaTree<T> where T: Encodable + {
             self.children.insert(child.key[0], child);
         }
     }
-
 }
 
 impl<'a, T> PartialEq for MerklePatriciaTree<T>
@@ -258,7 +267,7 @@ impl<'a, T> PartialEq for MerklePatriciaTree<T>
 impl<'a, T> Eq for MerklePatriciaTree<T> where T: Clone + Encodable {}
 
 
-//private functions test
+// private functions test
 #[cfg(test)]
 mod tests {
     extern crate rand;
