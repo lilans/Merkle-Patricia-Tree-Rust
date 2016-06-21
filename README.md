8fa823006159237df50ecbf30ad040a54014f370
Vladislav Lanskikh <nothingnesstobe@gmail.com>
2016-06-21 20:04:01 +0300

    update README.md

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-Merkle Patricia Tree
+# Merkle Patricia Tree
+
 The Merkle Patricia tree provides a persistent data structure to map between arbitrary-length
 binary data. The core of the trie, and its sole requirement in terms of the protocol
 specification is to provide a single 32-byte value that identifies a given set of key-value pairs.
--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -13,11 +13,6 @@ use crypto::sha2::Sha256;
 use bincode::rustc_serialize::encode;
 use bincode::SizeLimit;
 
-/// Merkle Patricia Tree
-/// The Merkle Patricia tree provides a persistent data structure to map between arbitrary-length
-/// binary data. The core of the trie, and its sole requirement in terms of the protocol
-/// specification is to provide a single 32-byte value that identifies a given set of key-value pairs.
-///
 /// Public Functios:
 /// fn new() -> MerklePatriciaTree<T> create new node
 /// fn get(&self, key: &'a [u8]) -> Option<&T> return value with a given key
